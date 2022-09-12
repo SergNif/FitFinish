80107bd333fa77449078a370ef793f80bbc0d300
SergNif <n777@bk.ru>
2022-09-12 19:18:50 +0300

    finish 1209 2

--- app/src/main/java/com/sergnfitness/android/fit/presentation/part3/Part3Page6MistakeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sergnfitness.android.fit.presentation.part3
-
-import androidx.lifecycle.ViewModel
-
-class Part3Page6MistakeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
